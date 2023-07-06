e537d9512dc65bc3d916efacd11a53eb2d7c2024
idzanoni <113716721+idzanoni@users.noreply.github.com>
2023-07-06 14:44:38 -0300

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: izanoni <izanoni@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 19:30:16 by izanoni           #+#    #+#             */
-/*   Updated: 2023/07/05 20:22:33 by izanoni          ###   ########.fr       */
+/*   Updated: 2023/07/06 13:25:37 by izanoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@
 
 int		ft_printf(const char *input, ...);
 int		ft_putchar_ret(char c);
-int		ft_putstr_ret(char *s);
-int		ft_putnbr_base_up(size_t nb);
-int		ft_putnbr_base_low(size_t nb);
-int		ft_putnbr_uns_ret(unsigned int n);
-int		ft_putnbr_ret(int n);
-int		ft_put_p(size_t nb);
-int		arg_check(va_list ap, const char *print, int i);
+int		ft_putstr_ret(char *str);
+int		ft_putnbr_base_up(size_t num);
+int		ft_putnbr_base_low(size_t num);
+int		ft_putnbr_uns_ret(unsigned int num);
+int		ft_putnbr_ret(int num);
+int		ft_put_p(size_t num);
+int		arg_check(va_list args, const char *print, int i);
 
 #endif
